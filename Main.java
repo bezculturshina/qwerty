d192e842ff99f9c811e064f9cc6cac5836554391
bezculturshina <144541430+bezculturshina@users.noreply.github.com>
2024-10-07 23:56:04 +0300

    1234567890

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("qwedfryu");
+        System.out.println("qwe00000000000000000000000000000000dfryu");
         System.out.println("qwedfryu465y39705673492805723498");
     }
 }
